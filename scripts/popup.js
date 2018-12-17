6c8380e42e78ec3dfa4d521cedd9a05c79725ff2
Ledyanoy <ledynaoy44@gmail.com>
2018-12-17 19:15:34 +0400

    add disabled button

--- scripts/popup.js
+++ scripts/popup.js
@@ -21,4 +21,11 @@ document.addEventListener('keydown', function (esc) {
     if(esc.keyCode === 27) {
         modal.classList.remove('modal--active')
     }
-}); 
+});
+
+function checkBtn() {
+  var input1 = document.getElementById('name');
+  var input2 = document.getElementById('pass'); 
+  var formButton = document.getElementById('form-button');
+  formButton.disabled = input1.value && input2.value ? false : "disabled";
+}  
